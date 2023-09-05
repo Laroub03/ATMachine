9cb3cfa500e0969f5ed529af8fdd647e1d0606c3
Laroub03 <98460971+Laroub03@users.noreply.github.com>
2023-09-05 09:59:59 +0200

    Add files via upload

---
+++ .vs/ATMachine/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMachine
+{
+    // Konto.cs
+    public class Account
+    {
+        public decimal Balance { get; private set; }
+
+        public Account(decimal initialBalance)
+        {
+            Balance = initialBalance;
+        }
+
+        public bool Withdraw(decimal amount)
+        {
+            if (amount <= 0 || amount > Balance)
+                return false;
+
+            Balance -= amount;
+            return true;
+        }
+    }
+
+}
