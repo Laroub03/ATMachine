6e59b1d2d269e76e15e44fde9d148867fa105315
Laroub03 <ayoublaroub@gmail.com>
2023-09-05 09:58:21 +0200

    Initial commit

---
+++ ATMachine.Tests/BankTests.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+using ATMachine;
+
+namespace ATMachine.Tests
+{
+    public class BankTests
+    {
+        [Fact]
+        public void TestWithdrawMoney_SuccessfulWithdraw()
+        {
+            Account account = new Account(1000);
+            PinValidator pinValidator = new PinValidator();
+
+            Bank bank = new Bank(account, pinValidator);
+
+            bool result = bank.WithdrawMoney("1234", 500);
+
+            Assert.True(result);
+            Assert.Equal(500, account.Balance);
+        }
+
+        [Fact]
+        public void TestWithdrawMoney_InvalidPin()
+        {
+            Account konto = new Account(1000);
+            PinValidator pinValidator = new PinValidator();
+
+            Bank bank = new Bank(konto, pinValidator);
+
+            bool result = bank.WithdrawMoney("5678", 500);
+
+            Assert.False(result);
+            Assert.Equal(1000, konto.Balance);
+        }
+
+        [Fact]
+        public void TestWithdrawMoney_InsufficientFunds()
+        {
+            Account konto = new Account(1000);
+            PinValidator pinValidator = new PinValidator();
+
+            Bank bank = new Bank(konto, pinValidator);
+
+            bool result = bank.WithdrawMoney("1234", 1500);
+
+            Assert.False(result);
+            Assert.Equal(1000, konto.Balance);
+        }
+    }
+}
